1d2b5276ef8e81296d48b56ec074d00e14bba4e1
Sebastian Medina <medinasebastian172@gmail.com>
2025-06-18 18:28:18 -0300

    Configure Django admin for Plant model

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Plant
 
-# Register your models here.
+@admin.register(Plant)
+class PlantAdmin(admin.ModelAdmin):
+    list_display = ("name", "min_temp", "max_temp", "min_humidity", "max_humidity")
+    prepopulated_fields = {"slug": ("name",)}
