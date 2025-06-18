acb0d742ac8229f791d7dcaf5591001a8a085b20
Sebastian Medina <medinasebastian172@gmail.com>
2025-06-18 18:28:25 -0300

    Add Plant serializers for API responses

---
+++ backend/api/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Plant
+
+class PlantSerializer(serializers.ModelSerializer):
+    value = serializers.CharField(source="slug")   
+    label = serializers.CharField(source="name")
+
+    class Meta:
+        model = Plant
+        fields = ("value", "label")
+
+#Si necesito mas detalles en el front
+class PlantDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plant
+        fields = ("id","name","slug","min_temp","max_temp","min_humidity","max_humidity","care_instructions")
